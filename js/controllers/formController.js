1cdd8f09be2f9bc229eae66857ac93f63b4305b9
techraiders <hello_navneet@hotmail.com>
2016-12-25 17:24:23 +0530

    Designed form.

--- js/controllers/formController.js
+++ js/controllers/formController.js
@@ -2,6 +2,11 @@
 	"use strict";
 	angular.module("nestedForm")
 		.controller("formController", function($scope) {
-			$scope.schoolRegistration = [];
+			$scope.schoolsList = [];
+			$scope.msg = "Hello";
+			console.info($scope.msg);
+			$scope.register = function(school) {
+				console.info("School Registered", $scope.schoolsList);
+			}
 		});
 })(window.angular);
